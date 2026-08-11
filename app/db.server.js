ef800ae2d60e2cf8780b66cf01800ec2c39dbd2d
syedhamzaimran31 <syedhamzaimran31@gmail.com>
2026-08-11 22:26:22 +0500

    fix: remove unnecessary blank line in db.server.js

--- app/db.server.js
+++ app/db.server.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
     global.prismaGlobal = new PrismaClient();
   }
+  
 }
 
 const prisma = global.prismaGlobal ?? new PrismaClient();
